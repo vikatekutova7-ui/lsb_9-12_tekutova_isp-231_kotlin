eb26e9aa5acb3e1b284e3213e7860d98ac0147f4
vikatekutova7-ui <vikatekutova7@gmail.com>
2026-02-08 16:08:12 +0300

    add delegation example

---
+++ src/example/Delegation.kt
@@ -0,0 +1,53 @@
+package example
+import kotlin.properties.Delegates
+interface Base {
+    fun someFun()
+}
+class BaseImpl() : Base {
+    override fun someFun() {}
+}
+class Derived(someBase: Base) : Base by someBase
+
+interface Messenger {
+    fun sendTextMessage()
+    fun sendVideoMessage()
+}
+
+interface PhotoDevice {
+    fun takePhoto()
+}
+
+class PhotoCamera : PhotoDevice {
+    override fun takePhoto() = println("Take a photo")
+
+}
+
+class InstantMessenger(val programName: String) : Messenger {
+    override fun sendTextMessage() = println("Send text message")
+    override fun sendVideoMessage() = println("Send video message")
+}
+
+class SmartPhone(val name: String, m: Messenger) : Messenger by m {
+    override fun sendTextMessage() = println("Send sms")
+}
+
+var counter: Int by Delegates.observable(0) { _, old, new ->
+    println("Счетчик изменился: $old -> $new")
+}
+
+class User {
+    var name: String by Delegates.observable("<no name>") { _, old, new ->
+        println("Имя изменено: $old -> $new")
+    }
+}
+
+fun main() {
+    val max = InstantMessenger(programName = "MAX")
+    val photoCamera = PhotoCamera()
+    val yotaPhone = SmartPhone(name = "YotaPhone ", m = max, p=photoCamera)
+    yotaPhone.sendVideoMessage()
+    yotaPhone.sendTextMessage()
+    counter = 1
+    counter=5
+
+}
